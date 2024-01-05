ecb8a95420620b4c910c86be89ff9638f37a46f9
faustinopsy <rodrigohipnose@gmail.com>
2024-01-05 19:37:00 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
-import express  from 'express';
-import bodyParser  from 'body-parser';
-import megaRoutes  from './App/Router/megaRoutes.js';
+import express from 'express';
+import bodyParser from 'body-parser';
+import megaRoutes from './App/Router/megaRoutes.js';
+import debug from 'debug';
+import cluster from 'cluster';
+import os from 'os';
 
+const cpus = os.cpus();
+const debugLog = debug('server.js');
 const app = express();
 app.use(bodyParser.json());
+
 
 app.use('/app', megaRoutes);
 
 const port = 80;
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
+
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
+
+  cpus.forEach(_ => {
+    const worker = cluster.fork();
+    console.log(`Worker ${worker.process.pid} started`);
+  });
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+    debconsole.logugLog('Starting a new worker');
+    cluster.fork();
+  });
+} else {
+  app.listen(port, () => console.log(`Worker ${process.pid} started, server listening on port ${port}`));
+}
